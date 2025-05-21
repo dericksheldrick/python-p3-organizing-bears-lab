5f3b912d4429d9c9bbb26eae49f10d79f78e1694
Derick Sheldrick <sheldrickderick9@gmail.com>
2025-05-21 15:02:04 +0300

    assignment submission

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name, sex)
+VALUES
+    ("Mr. Chocolate", "M"),
+    ("Rowdy", "M"),
+    ("Tabitha", "M"),
+    ("Sergeant Brown", "F"),
+    ("Melissa", "F"),
+    ("Grinch", "F"),
+    ("Wendy", "F"),
+    (NULL, "F");
